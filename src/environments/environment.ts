06dd091809719f071a6023fd6e58a2612bb3d0fd
opeAlomaja <alomajaopemipo8@gmail.com>
2022-11-06 20:53:39 +0100

    ok

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   forumAPI: 'http://app.deltastatepensionsbureau.com/IBHelpDeskWebAPI/api/ForumTypes',
   conversationsAPI: 'http://app.deltastatepensionsbureau.com/IBHelpDeskWebAPI/api/Conversations/GetConversations',
   commentAPI: 'http://app.deltastatepensionsbureau.com/IBHelpDeskWebAPI/api/Comments',
-  memberAPI: 'https://app.deltastatepensionsbureau.com/IBHelpDeskWebAPI/api/Auth/Login',
+  memberAPI: 'http://app.deltastatepensionsbureau.com/IBHelpDeskWebAPI/api/Members/',
   supportAPI: 'http://app.deltastatepensionsbureau.com/IBHelpDeskWebAPI/api/Tickets/',
   supportAPI2: 'http://app.deltastatepensionsbureau.com/IBHelpDeskWebAPI/api/Tickets',
   supportAPI3: 'http://app.deltastatepensionsbureau.com/IBHelpDeskWebAPI/api/Conversations/PostConversation',
